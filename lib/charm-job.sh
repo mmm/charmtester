3111d862f5db5275d815dfe4fd404bab977cb117
Mark Mims <mark.mims@canonical.com>
2012-02-17 11:10:34 -0700

    publisher plugins

--- lib/charm-job.sh
+++ lib/charm-job.sh
@@ -20,12 +20,16 @@ create_job_for_charm() {
   local home=$3
   local job_name=$(job_name_for_charm $charm_name)
   local API_TOKEN=$(get_api_token $home)
+  local publisher_channel=$(config-get publisher_channel)
+  local publisher_site=$(config-get publisher_site)
+  local publisher_username=$(config-get publisher_username)
+  local publisher_password=$(config-get publisher_password)
   mkdir -p -m755 $home/jobs/$job_name
   ch_template_file 755 \
                    $user.$user \
                    job-config.xml \
                    $home/jobs/$job_name/config.xml \
-                   "user home charm_name job_name API_TOKEN"
+                   "user home charm_name job_name API_TOKEN publisher_channel publisher_site publisher_username publisher_password"
 }
 
 blacklisted_charm() {
