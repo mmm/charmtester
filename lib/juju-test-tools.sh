680b2ee73ae8f5816a276492945eaba4789dcd95
Mark Mims <mark.mims@canonical.com>
2012-04-09 08:48:12 -0600

    add test scheduler

--- lib/juju-test-tools.sh
+++ lib/juju-test-tools.sh
@@ -2,6 +2,17 @@
 
 [ -f lib/ch-file.sh ] && . lib/ch-file.sh
 
+install_test_scheduler() {
+  local user=$1
+  local home=$2
+
+  mkdir -p -m755 $home/bin
+  ch_install_file 755 $user:nogroup run-charm-jobs $home/bin/
+
+  mkdir -p -m755 $home/etc
+  ch_install_file 755 $user:nogroup crontab $home/etc/
+}
+
 install_installation_runner() {
   local user=$1
   local home=$2
@@ -33,6 +44,9 @@ cache_juju_environment() {
 install_juju_test_tools() {
   local user=$1
   local home=$2
+
+  juju-log "installing test scheduler"
+  install_test_scheduler $user $home
 
   juju-log "installing local runner"
   install_installation_runner $user $home
