88c7d85fa6445765434817537b732d19e9b6d3fb
Mark Mims <mark.mims@canonical.com>
2012-04-27 08:47:28 -0600

    test wrapper

--- lib/juju-test-tools.sh
+++ lib/juju-test-tools.sh
@@ -18,6 +18,14 @@ install_installation_runner() {
   local home=$2
 
   mkdir -p -m755 $home/bin
+  ch_install_file 755 $user:nogroup charm-test $home/bin/
+}
+
+install_installation_runner() {
+  local user=$1
+  local home=$2
+
+  mkdir -p -m755 $home/bin
   ch_install_file 755 $user:nogroup charm-installation-test $home/bin/
 }
 
@@ -40,6 +48,9 @@ install_juju_test_tools() {
 
   juju-log "installing test scheduler"
   install_test_scheduler $user $home
+
+  juju-log "installing test wrapper"
+  install_test_wrapper $user $home
 
   juju-log "installing local runner"
   install_installation_runner $user $home
