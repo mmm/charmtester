3bc9a05d8a69e0312484ba07d17f026de666a79f
Mark Mims <mark.mims@canonical.com>
2012-05-02 04:45:02 -0600

    turn off host checking for testing

--- lib/ch-user.sh
+++ lib/ch-user.sh
@@ -7,11 +7,22 @@ make_user_sudo() {
   chmod 0440 $sudoer_file
 }
 
+turn_off_host_key_checking() {
+  local user=$1
+  local home=$2
+  local ssh_config_file="$home/.ssh/config"
+  echo "StrictHostKeyChecking no" > $ssh_config_file
+  chown $user:nogroup $ssh_config_file
+  chmod 0400 $ssh_config_file
+}
+
 generate_ssh_keys() {
   local user=$1
   local home=$2
   if [ ! -f $home/.ssh/id_rsa ]; then
     su -l $user -c "ssh-keygen -q -N '' -t rsa -b 2048 -f $home/.ssh/id_rsa"
   fi
+
+  turn_off_host_key_checking $user $home
 }
 
